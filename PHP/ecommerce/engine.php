994f7f094f1d001552c110383b990d70757e4c57
Nelson Bassey <nellybaz10@gmail.com>
2019-04-16 12:54:27 +0200

    added php files for backend

---
+++ PHP/ecommerce/engine.php
@@ -0,0 +1,58 @@
+<?php
+
+
+//actions
+    //get all products
+    //add product to cart
+    //show all added products
+    //checkout product/products
+
+
+include "functions.php";
+
+//check to see if our web app is the one sending the request
+if(isset($_POST["token"]) == "webDevGroupTimiErastusOlivierNelson"){
+
+    $tag = $_POST['tag'];
+
+    switch ($tag) {
+        case 'getProducts':
+            $res = getProducts();
+
+            echo json_encode($res);
+
+            break;
+
+
+        case 'addProductToCart':
+            $productID = $_POST['productID'];
+            $userID = $_POST['userID'];
+            $res = addToCart($productID, $userID);
+
+            echo json_encode($res);
+
+            break;
+
+        case 'showProductsInCart':
+            $userID = $_POST['userID'];
+            $res = showCart($userID);
+
+            echo json_encode($res);
+
+            break;
+
+
+        case 'checkOutProducts':
+            $userID = $_POST['userID'];
+            $products = $_POST['productID_array'];
+            $res = checkout($userID, $products);
+
+            echo json_encode($res);
+
+            break;
+        
+        default:
+            # code...
+            break;
+    }
+}
---
+++ PHP/ecommerce/functions.php
@@ -0,0 +1,76 @@
+<?php
+
+include "db.php";
+
+function getProducts(){
+    $return_arr = array();
+    $query = "select * from products";
+
+    $result = mysqli_query($conn, $query);
+    if(mysqli_num_rows($result)  > 0){
+        while ($row = mysqli_fetch_assoc($result)) {
+            $row_array['id'] = $row['id'];
+            $row_array['col1'] = $row['col1'];
+            $row_array['col2'] = $row['col2'];
+        
+            array_push($return_arr,$row_array);
+           }
+    }
+
+    mysqli_close($conn);
+    return $return_arr;
+}
+
+
+
+function addToCart($productId, $userID){
+    $stmt = $conn->prepare('INSERT INTO cart (productID, userID) VALUES (?,?)');
+
+    if (
+		$stmt &&
+		$stmt -> bind_param('ss', $productId, $userID) &&
+		$stmt -> execute()
+	) {
+         // new item added to cart
+         return 1;
+    }
+    
+    else{
+        return 0;
+    }
+}
+
+
+function showCart($userID){
+    $return_arr = array();
+    $stmt = $conn -> prepare('SELECT name, price FROM products WHERE productID IN (SELECT productID FROM cart WHERE userID = ?)');
+
+    if (
+        $stmt &&
+        $stmt -> bind_param('s', $userID) &&
+        $stmt -> execute() &&
+        $stmt -> store_result() &&
+        $stmt -> bind_result($name, $price)
+    ) {
+    
+        while ($stmt -> fetch()) {
+            echo "$name: $price <br>";
+
+            $res = ["name" => $name, "price" => $price];
+
+            array_push($return_arr, $res);
+        }
+
+        return $return_arr;
+    
+    } else {
+        echo 'Prepared Statement Error';
+        return 0;
+    }
+}
+
+
+function checkout($userID, $products){
+    //either update cart table or 
+    //delete from cart table and add it in checkout table
+}
